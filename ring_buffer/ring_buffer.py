dce0b3c8c488b61ed46d1027823073ec2be0b4db
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-04-24 10:20:42 +0200

    ring_buffer: works and passes both tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,27 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.head
+        else:
+            self.current.value = item
+            if self.current == self.storage.tail:
+                self.current = self.storage.head
+            else:
+                self.current = self.current.next
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+
+        node = self.storage.head
+
+        while node:
+            list_buffer_contents.append(node.value)
+            node = node.next
 
         return list_buffer_contents
 
@@ -23,10 +37,67 @@ class RingBuffer:
 
 class ArrayRingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.current = 0 
+        self.storage = [None] * self.capacity
 
     def append(self, item):
-        pass
+        if len(self.storage) < self.capacity:
+            self.storage.append(item)
+            self.current += 1
+        else: 
+            self.current += 1
+            self.storage[(self.current % self.capacity) -1] = item
 
     def get(self):
-        pass
+        content = []
+
+        for e in self.storage:
+            if e != None:
+                content.append(e)
+        
+        return content
+        
+
+
+buffer = RingBuffer(3)
+
+print(buffer.get())   # should return []
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+
+print(buffer.get())   # should return ['a', 'b', 'c']
+
+# 'd' overwrites the oldest value in the ring buffer, which is 'a'
+buffer.append('d')
+
+print(buffer.get())   # should return ['d', 'b', 'c']
+
+buffer.append('e')
+buffer.append('f')
+
+print(buffer.get())   # should return ['d', 'e', 'f']
+
+# --------------------------------------------------------------------
+
+buffer2 = ArrayRingBuffer(3)
+
+print(buffer2.get())   # should return []
+
+buffer2.append('a')
+buffer2.append('b')
+buffer2.append('c')
+
+print(buffer2.get())   # should return ['a', 'b', 'c']
+
+# 'd' overwrites the oldest value in the ring buffer, which is 'a'
+buffer2.append('d')
+
+print(buffer2.get())   # should return ['d', 'b', 'c']
+
+buffer2.append('e')
+buffer2.append('f')
+
+print(buffer2.get())   # should return ['d', 'e', 'f']
